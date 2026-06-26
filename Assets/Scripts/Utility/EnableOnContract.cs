a22008630b5172d48dea4de7b8a8eae52239870d
Semih Yılmaz <semihyilmaz032@gmail.com>
2026-06-26 04:04:31 +0300

    Tutorial thingy

---
+++ Assets/Scripts/Utility/EnableOnContract.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class EnableOnContract : MonoBehaviour
+{
+    [Header("Object to enable")]
+    [SerializeField] private GameObject objectToEnable;
+
+    [Header("How long should the object be enabled for?")]
+    [SerializeField] private float duration;
+
+    private Coroutine EnablingRoutine = null;
+
+    void OnTriggerEnter2D(Collider2D _)
+    {
+        if (EnablingRoutine != null)
+            StopCoroutine(EnablingRoutine);
+        
+        EnablingRoutine = StartCoroutine(EnableObject(duration));
+    }
+
+    IEnumerator EnableObject(float duration)
+    {
+        objectToEnable.SetActive(true);
+
+        yield return new WaitForSeconds(duration);
+
+        objectToEnable.SetActive(false);
+    }
+}
